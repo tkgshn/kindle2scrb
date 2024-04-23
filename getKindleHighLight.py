07fb38de02371a1eff9da79ca43e586e0fda1e6c
tkgshn <takagishunsuke1129@gmail.com>
2024-04-23 14:09:32 +0900

    bug

--- getKindleHighLight.py
+++ getKindleHighLight.py
@@ -24,38 +24,22 @@ import csv
 import json
 import collections as cl
 
+from webdriver_manager.chrome import ChromeDriverManager
 
 def main():
     USER = os.getenv('KINDLE_USER')
     PASS = os.getenv('KINDLE_PASS')
 
-
-
-    # options = Options()
-    # options.binary_location = '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
-    # options.add_argument('--headless')
-    # options.add_argument('--disable-gpu')
-    # driver = webdriver.Chrome(chrome_options=options)
-    # UserAgent = "Safari/537.36 Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66"
-    # driver = webdriver.PhantomJS(
-    #     desired_capabilities={'phantomjs.page.settings.userAgent': UserAgent,
-    #     },
-    # )
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': 'ja'})
-    # options.binary_location = '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
-    # driver = webdriver.Chrome(chrome_options=options)
-    driver = webdriver.Chrome('/usr/local/bin/chromedriver')
-    # driver.set_window_size(1920, 1080)
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     print('drive start')
-    # url = 'https://www.amazon.co.jp/ap/signin?clientContext=375-4875962-6582025&openid.return_to=https%3A%2F%2Fread.amazon.co.jp%2Fkp%2Fnotebook%3FamazonDeviceType%3DA2CLFWBIMVSE9N&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=amzn_kp_jp&openid.mode=checkid_setup&marketPlaceId=A1VC38T7YXB528&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&pageId=amzn_kp_notebook_us&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.pape.max_auth_age=1209600&siteState=clientContext%3D355-3088953-6443912%2CsourceUrl%3Dhttps%253A%252F%252Fread.amazon.co.jp%252Fkp%252Fnotebook%253FamazonDeviceType%253DA2CLFWBIMVSE9N%2Csignature%3DSZXy31cn2x1mpyj2FotQcZRR2j2Bq3gj3D&language=ja_JP&auth=Customer+is+not+authenticated'
     url = 'https://read.amazon.co.jp/kp/notebook?amazonDeviceType=A2CLFWBIMVSE9N'
     driver.get(url)
     sleep(3)
 
     uid = driver.find_element_by_id('ap_email')
     upw = driver.find_element_by_id('ap_password')
-
 
     uid.send_keys(USER)
     upw.send_keys(PASS)
@@ -65,7 +49,6 @@ def main():
 
     print('url:', driver.current_url)
 
-    # amazon.com may be accessed only 4 times in an hour by phantomjs.
     if not driver.current_url=='https://read.amazon.co.jp/kp/notebook?amazonDeviceType=A2CLFWBIMVSE9N':
         print(' はいれなかったー　')
         driver.save_screenshot('reject.png')
@@ -87,13 +70,11 @@ def main():
 
     titlesThatHasNewHighlight = []
     for obj in objs:
-        # memoContents = []
         memoContents = cl.OrderedDict()
         print('  obj.id:', obj['id'])
         idname = 'div#'+str(obj['id'])+' > span > a'
         print('  idname:', idname)
 
-        # road time
         sleep(5)
         driver.find_element_by_css_selector(idname).click()
         sleep(10)
@@ -139,12 +120,6 @@ def main():
     with open('./assets/last.json', 'w') as f:
         json.dump(lastcl, f, indent=4, ensure_ascii=False)
 
-    # with open('./assets/last.csv', 'w') as f:
-    #     writer = csv.writer(f, lineterminator='\n')
-    #     forjsonsave = [toDate] + titlesThatHasNewHighlight
-    #     writer.writerow(fortmp)
-
-
     driver.close()
     driver.service.process.send_signal(signal.SIGTERM)
     driver.quit()
@@ -153,8 +128,6 @@ def main():
     print('do pushscrbbyhlesschrome.py')
 
     os.system('ls -la')
-    # os.system('sudo mv ./assets/last.json /var/www/html/')
-    # os.system('sudo mv ./assets/highlight /var/www/html/')
     sleep(10)
     os.system('python pushscrbbyhlesschrome.py')
 
